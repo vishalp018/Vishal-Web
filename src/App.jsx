26f5a6cbded0fcf6342e4e34000ed873c6b537ab
VISHAL <palvishal111@gmail.com>
2025-06-01 20:12:17 +0530

    Fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
-import { SpeedInsights } from "@vercel/speed-insights/next"
-=======
-import { SpeedInsights } from "@vercel/speed-insights/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"; // or .next — pick one
 import Navbar from "./components/Navbar/Navbar";
 import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
@@ -16,11 +13,10 @@ import BlurBlob from './components/BlurBlob';
 const App = () => {
   return (
     <div className="bg-[#050414]">
-
-       <BlurBlob position={{ top: '35%', left: '20%' }} size={{ width: '30%', height: '40%' }} />
+      <BlurBlob position={{ top: '35%', left: '20%' }} size={{ width: '30%', height: '40%' }} />
 
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
-      
+
       <div className="relative pt-20">
         <Navbar />
         <About />
@@ -31,42 +27,6 @@ const App = () => {
         <Contact />
         <Footer />
       </div>
-
-    </div>
-  );
-};
-
-export default App;
->>>>>>> 61fedf1a175be78b7ba5819cb53e229fe50647ea
-import Navbar from "./components/Navbar/Navbar";
-import About from "./components/About/About";
-import Skills from "./components/Skills/Skills";
-import Experience from "./components/Experience/Experience";
-import Work from "./components/Work/Work";
-import Education from "./components/Education/Education";
-import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/Footer";
-import BlurBlob from './components/BlurBlob';
-
-const App = () => {
-  return (
-    <div className="bg-[#050414]">
-
-       <BlurBlob position={{ top: '35%', left: '20%' }} size={{ width: '30%', height: '40%' }} />
-
-      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
-      
-      <div className="relative pt-20">
-        <Navbar />
-        <About />
-        <Skills />
-        <Experience />
-        <Work />
-        <Education />
-        <Contact />
-        <Footer />
-      </div>
-
     </div>
   );
 };
